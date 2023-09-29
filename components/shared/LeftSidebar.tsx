9ffbc75b3481abcac03b0de0ace11170faa66f3b
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2023-09-29 23:17:42 +0500

    chore: designed and implemented the profile page and show the threads on it chore: implemented the search page and form chore: created the required components for these pages

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -6,11 +6,12 @@ import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 
 import { sidebarLinks } from "@/constants";
-import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
 
 function LeftSidebar() {
   const router = useRouter();
   const pathname = usePathname();
+  const { userId } = useAuth();
 
   return (
     <section className="custom-scrollbar leftsidebar">
@@ -19,6 +20,8 @@ function LeftSidebar() {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
+
+          if (link.route === "/profile") link.route = `${link.route}/${userId}`;
 
           return (
             <Link
