7d2ce9d432302f739387c9583bc3e56ebe572e90
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2023-09-30 12:44:08 +0500

    chore: design and implemented the communities page and it's functionality

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,12 +1,12 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-// import Searchbar from "@/components/shared/Searchbar";
-// import Pagination from "@/components/shared/Pagination";
-// import CommunityCard from "@/components/cards/CommunityCard";
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
+import CommunityCard from "@/components/cards/CommunityCard";
 
 import { fetchUser } from "@/lib/actions/user.actions";
-// import { fetchCommunities } from "@/lib/actions/community.actions";
+import { fetchCommunities } from "@/lib/actions/community.actions";
 
 async function Page({
   searchParams,
@@ -19,23 +19,23 @@ async function Page({
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
-  //   const result = await fetchCommunities({
-  //     searchString: searchParams.q,
-  //     pageNumber: searchParams?.page ? +searchParams.page : 1,
-  //     pageSize: 25,
-  //   });
+  const result = await fetchCommunities({
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
 
   return (
     <>
       <h1 className="head-text">Communities</h1>
 
-      {/* <div className='mt-5'>
-        <Searchbar routeType='communities' />
+      <div className="mt-5">
+        <Searchbar routeType="communities" />
       </div>
 
-      <section className='mt-9 flex flex-wrap gap-4'>
+      <section className="mt-9 flex flex-wrap gap-4">
         {result.communities.length === 0 ? (
-          <p className='no-result'>No Result</p>
+          <p className="no-result">No Result</p>
         ) : (
           <>
             {result.communities.map((community) => (
@@ -54,10 +54,10 @@ async function Page({
       </section>
 
       <Pagination
-        path='communities'
+        path="communities"
         pageNumber={searchParams?.page ? +searchParams.page : 1}
         isNext={result.isNext}
-      /> */}
+      />
     </>
   );
 }
